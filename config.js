13ec876069615785cf6a83fa69b950cc0f3e87d7
Nathan Hoad <nathan@nathanhoad.net>
2019-05-23 20:13:08 +1000

    Extract compiler into a module

--- config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  POSTS_PATH: path.join(__dirname, 'posts'),
-  PUBLIC_PATH: path.join(__dirname, 'public'),
-  TEMPLATES_PATH: path.join(__dirname, 'templates')
-};
